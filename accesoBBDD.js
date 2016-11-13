5c1c48cdbb7d877b1bbf309a63ad1ea4575d5df8
Olayer <Olayer@CASA>
2016-11-14 00:12:58 +0100

    acceso a la BBDD

---
+++ accesoBBDD.js
@@ -0,0 +1,121 @@
+// Conexion a la BBDD
+"use strict";
+
+var mysql = require("mysql");
+var query;
+var resultado;
+var conexion = mysql.createConnection({
+    host:  "localhost",
+    user:  "root",
+    password: "root",
+    database: "saboteur"
+});
+
+var funcionQuery=function(err, rows) 
+                            {
+                                if (err) 
+                                {
+                                    console.error(err);
+                                    resultado=undefined;
+                                } 
+                                else 
+                                {
+                                    resultado=rows;
+                                }
+                            };
+var accion=function (err) 
+            {
+                if (err) 
+                {
+                    console.error(err);
+                } 
+                else 
+                {
+                    conexion.query(query,funcionQuery);
+                }
+                conexion.end();
+            }
+function entrada(acc)
+{
+    switch(acc)
+    {
+        //Acciones sobre usuarios(crear, autenticar modificar,etc)
+        case "autenticar":
+            query="SELECT Nick,Contarseña " + 
+                  "FROM Usuarios";
+            //Conectamos con la consulta requerida
+            conexion.connect(accion);
+            break;
+        case "crear_usuario":
+            if(valores!=null && valores.length===7)
+            {
+                query="INSERT INTO Usuarios"+
+                      "VALUES (null, " + valores.nick +","+
+                                       + valores.nombre +","+
+                                       + valores.apellidos +","+
+                                       + valores.contraseña +","+
+                                       + valores.fechaNac +","+
+                                       + valores.sexo +","+
+                                       + valores.imagen +")";
+                //Conectamos con la consulta requerida
+                conexion.connect(accion);
+            }
+            else
+            {
+                resultado=undefined;
+            }
+            break;
+        case "modificar_usuario":
+            if(valores!=null && valores.length===7)
+            {
+                query="UPDATE Usuarios"+
+                      "SET  Nick=" + valores.nick +","+
+                           "Nombre=" + valores.nombre +","+
+                           "Apellidos="+ valores.apellidos +","+
+                           "Contraseña"+ valores.contraseña +","+
+                           "Fecha_Nac="+ valores.fechaNac +","+
+                           "Sexo="+ valores.sexo +","+
+                           "Imagen="+ valores.imagen;
+                //Conectamos con la consulta requerida
+                conexion.connect(accion);
+            }
+            else
+            {
+                resultado=undefined;
+            }
+            break;
+        case "partidas_usuario":
+            break;
+        //No se si sera necearia(intuyo que no)
+        case "salir":
+            break;
+        // acciones sobre Partidas
+        case "crear_partida":
+            break;
+        case "borrar_partida":
+            break;
+        case "modificar_partida":
+            break;
+        case "usuarios_partida":
+            break;
+        //Aciones sobre los tableros guardados
+        case "extraer_tablero":
+            query="SELECT * " + 
+                  "FROM tableros"+
+                  "WHERE ID_Partida ="+valor;
+            //Conectamos con la consulta requerida
+            conexion.connect(accion);
+            break;
+        default:
+            console.log("cosulta no encontrada");
+    }
+    return resultado;
+}
+ function ejecutarQuery()
+    {
+        entrada(acc,valores);
+        return resultado;
+    }
+module.request(ejecutarQuery);
+
+
