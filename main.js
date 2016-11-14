0a83e52d58a69bfb55a222768208bb2ad0005cbd
Olayer <Olayer@CASA>
2016-11-15 00:02:20 +0100

    conexion del servidor mas dependecias en el package para ejs

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+"use strict";
+
+var http = require("http");
+var servidor = http.createServer(function(request, response) {
+    console.log(`Método: ${request.method}`);
+    console.log(`URL: ${request.url}`);
+    console.log(request.headers);
+});
+servidor.listen(3000, function(err) {
+    if (err) {
+        console.log("Error al abrir el puerto 3000: " + err);
+    } else {
+        console.log("Servidor escuchando en el puerto 3000.");
+    }
+});
+
 
 
